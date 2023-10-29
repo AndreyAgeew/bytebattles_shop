bd8196153371eeffb1c280459d4aaba63481f180
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-30 00:12:56 +0300

    Add validate for phone and password

---
+++ auth/errors.py
@@ -0,0 +1,6 @@
+class PasswordValidationError(Exception):
+    pass
+
+
+class PhoneValidationError(Exception):
+    pass
--- auth/manager.py
+++ auth/manager.py
@@ -1,10 +1,11 @@
-import uuid
+import re
 from typing import Optional
 
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
 from auth.database import User, get_user_db
+from auth.errors import PhoneValidationError, PasswordValidationError
 
 SECRET = "SECRET"
 
@@ -12,6 +13,21 @@ SECRET = "SECRET"
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
+
+    def __validate_password(self, password: str):
+        if len(password) < 8:
+            raise PasswordValidationError("Password is too short")
+        if not any(c.isalpha() and c.isascii() for c in password):
+            raise PasswordValidationError("Password must contain Latin characters")
+        if not any(c.isupper() for c in password):
+            raise PasswordValidationError("Password must contain at least one uppercase letter")
+        if not any(c in '$%&!:.' for c in password):
+            raise PasswordValidationError("Password must contain at least one of $%&!:.")
+
+    def __validate_phone(self, phone: str):
+        phone_pattern = re.compile(r'^\+7\d{10}$')
+        if not phone_pattern.match(phone):
+            raise PhoneValidationError("Invalid phone number")
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
@@ -34,6 +50,8 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
             else user_create.create_update_dict_superuser()
         )
         password = user_dict.pop("password")
+        self.__validate_password(password)
+        self.__validate_phone(user_dict['phone_number'])
         user_dict["hashed_password"] = self.password_helper.hash(password)
         user_dict["role_id"] = 1
         created_user = await self.user_db.create(user_dict)
