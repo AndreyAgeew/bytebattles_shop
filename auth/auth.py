df3f9a45cb9214a3d101b17d30f3097648a5c172
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-29 12:14:17 +0300

    rename SECRET to JTWTS_KEY

--- auth/auth.py
+++ auth/auth.py
@@ -2,15 +2,15 @@ from fastapi_users.authentication import CookieTransport, AuthenticationBackend
 
 from fastapi_users.authentication import JWTStrategy
 
-from config import SECRET
+from config import JTWTS_KEY
 
 cookie_transport = CookieTransport(cookie_max_age=3600)
 
-SECRET = SECRET
+JTWTS_KEY = JTWTS_KEY
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=JTWTS_KEY, lifetime_seconds=3600)
 
 
 auth_backend = AuthenticationBackend(
