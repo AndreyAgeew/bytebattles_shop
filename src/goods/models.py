2e7b1eb3845f810641d89b8981b9072358f65446
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-30 21:57:54 +0300

    Add table Goods

---
+++ src/goods/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean, DECIMAL
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class Goods(Base):
+    """
+        Модель товара в базе данных.
+
+        Атрибуты:
+            id (Column): Уникальный идентификатор товара.
+            name (Column): Название товара.
+            price (Column): Цена за единицу товара.
+            quantity (Column): Количество доступного товара.
+            updated_at (Column): Метка времени обновления товара.
+            is_active (Column): Признак допуска на продажу товара.
+    """
+    __tablename__ = "goods"
+
+    id = Column(Integer, primary_key=True, doc="Уникальный идентификатор товара.")
+    name = Column(String, nullable=False, doc="Название товара")
+    price = Column(DECIMAL, nullable=False, doc="Цена за единицу товара")
+    quantity = Column(Integer, nullable=False, doc="Количество доступного товара")
+    updated_at = Column(TIMESTAMP, default=datetime.utcnow(), doc="Метка времени обновления товара")
+    is_active = Column(Boolean, default=True, doc="Признак допуска на продажу товара")
