2103bfefc2ea3b665a580f363ccf7e0b2abf4351
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-30 12:49:55 +0300

    Add SUserAuth for auth login

--- auth/schemas.py
+++ auth/schemas.py
@@ -29,3 +29,8 @@ class UserCreate(schemas.BaseUserCreate):
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
+
+
+class SUserAuth(schemas.BaseModel):
+    email_or_phone: str
+    password: str
