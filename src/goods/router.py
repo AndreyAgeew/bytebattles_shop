8b4ed6b429d2ca2cb9c8b45914747449aea14c48
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-01 00:09:56 +0300

    Add user: User = Depends(get_current_user) for routers

--- src/goods/router.py
+++ src/goods/router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from auth.dependecies import get_current_user
+from auth.models import User
 from database import get_async_session
 from goods.models import Goods
 from goods.schemas import GoodsCreate
@@ -12,14 +15,16 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_activity_goods(session: AsyncSession = Depends(get_async_session)):
+async def get_activity_goods(user: User = Depends(get_current_user),
+                             session: AsyncSession = Depends(get_async_session)):
     query = select(Goods.name).where(Goods.is_active == True)
     result = await session.execute(query)
     return [row[0] for row in result]
 
 
 @router.post("/")
-async def add_goods(goods_data: GoodsCreate, session: AsyncSession = Depends(get_async_session)):
+async def add_goods(goods_data: GoodsCreate, user: User = Depends(get_current_user),
+                    session: AsyncSession = Depends(get_async_session)):
     stmt = insert(Goods).values(**goods_data.dict())
     await session.execute(stmt)
     await session.commit()
