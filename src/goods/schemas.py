317500d0fe5d8807143934b223ad3f2fe04e559f
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-31 12:44:09 +0300

    Create schema GoodsCreate for Goods model

---
+++ src/goods/schemas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from _decimal import Decimal
+from pydantic import BaseModel
+
+
+class GoodsCreate(BaseModel):
+    id: int
+    name: str
+    price: Decimal
+    quantity: int
+    updated_at: datetime
+    is_active: bool
