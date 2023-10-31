9f917437682462a9e2dc6fd55644b34d4d0aee5b
Andrey Ageew <faceofdeviler@gmail.com>
2023-10-31 13:25:38 +0300

    Add base CORS settings

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from auth.base_config import fastapi_users
 from auth.schemas import UserRead, UserCreate
@@ -7,6 +8,21 @@ from auth.router import router as login
 from goods.router import router as goods
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
