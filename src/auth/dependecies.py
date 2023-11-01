3b58855bb9280e0e61ca99083b0fd57b30daade7
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-01 19:36:39 +0300

    Add detail in HTTPException

--- src/auth/dependecies.py
+++ src/auth/dependecies.py
@@ -10,30 +10,31 @@ from config import JWT_KEY, ALGORITHM_JWT
 def get_token(request: Request):
     token = request.cookies.get("goods_access_token")
     if not token:
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=401, detail="Access token is missing. You are not authenticated.")
     return token
 
 
 async def get_current_user(token: str = Depends(get_token)):
     try:
-        payload = jwt.decode(
-            token, JWT_KEY, ALGORITHM_JWT
-        )
-    except:
-        raise HTTPException(status_code=401)
-    expire: str = payload.get("exp")
+        payload = jwt.decode(token, JWT_KEY, ALGORITHM_JWT)
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired. Please re-authenticate.")
+    except jwt.DecodeError:
+        raise HTTPException(status_code=401, detail="Token decoding error. Please re-authenticate.")
+    expire = payload.get("exp")
     if not expire or int(expire) < datetime.utcnow().timestamp():
-        raise HTTPException(status_code=401)
-    user_id: str = payload.get("sub")
+        raise HTTPException(status_code=401, detail="Token has expired. Please re-authenticate.")
+    user_id = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=401, detail="User identifier is missing in the token. Please re-authenticate.")
     user = await UserDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=401)
+        raise HTTPException(status_code=401, detail="User with the specified identifier not found.")
     return user
 
 
 async def get_current_admin_user(current_user: User = Depends(get_current_user)):
     role_name = await UserDAO.find_user_role_name(current_user.id)
     if role_name == 'user':
-        raise HTTPException(status_code=403)
+        raise HTTPException(status_code=403, detail="You do not have sufficient permissions to perform this operation.")
+    return current_user
