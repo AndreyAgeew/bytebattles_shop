d860c50daf178eadb2665458486d8296745f0e08
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-05 01:14:20 +0300

    Update add_image_to_goods add commit

--- src/images/router.py
+++ src/images/router.py
@@ -15,8 +15,8 @@ router = APIRouter(
 
 @router.post("/add_to_goods/{goods_id}/")
 async def add_image_to_goods(goods_id: int, image: UploadFile, user: User = Depends(get_current_admin_user),
-                             db: AsyncSession = Depends(get_async_session)):
-    existing_goods = await GoodsDAO.find_by_id(db, goods_id)
+                             session: AsyncSession = Depends(get_async_session)):
+    existing_goods = await GoodsDAO.find_by_id(session, goods_id)
     if existing_goods is None:
         raise HTTPException(status_code=404, detail="Товар не найден")
 
@@ -25,6 +25,7 @@ async def add_image_to_goods(goods_id: int, image: UploadFile, user: User = Depe
 
     # Обновите поле image_url товара в базе данных, используя ваш новый метод
     image_url = f"src/static/img/goods/{image.filename}"
-    await GoodsDAO.update_goods_image(db, goods_id, image_url)
+    await GoodsDAO.update_goods_image(session, goods_id, image_url)
+    await session.commit()
 
     return {"filename": image.filename}
