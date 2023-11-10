a91294c915ed7f80928e6254f2024c268cdb0edc
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-10 23:28:55 +0300

    Added a script that fills the database (python src/commands/fill_goods_table.py )

---
+++ src/commands/fill_goods_table.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import asyncio
+import click
+
+current_file = os.path.abspath(__file__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(current_file))
+
+sys.path.insert(0, BASE_DIR)
+from commands.data import goods_data, roles_data, users_data
+from database import get_async_session
+from goods.dao import GoodsDAO
+from auth.dao import UserDAO
+
+src_dir = os.path.join(os.path.dirname(__file__), '..', '..')
+sys.path.append(src_dir)
+
+
+async def main():
+    try:
+        async for session in get_async_session():
+            await GoodsDAO.clear_goods_table(session)
+            await session.commit()
+            await UserDAO.clear_role_table(session)
+            await session.commit()
+            await UserDAO.clear_user_table(session)
+            await session.commit()
+
+            for goods, role, user in zip(goods_data, roles_data, users_data):
+                await GoodsDAO.add_all_goods(session, goods)
+                await UserDAO.add_all_roles(session, role)
+                await UserDAO.add_all_users(session, user)
+            await session.commit()
+            click.echo("Таблицы успешно заполнены.")
+    except Exception as e:
+        click.echo(f"Произошла ошибка: {e}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
