4194f9fcf71fc0e9f32533e85908b8ce22721a58
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-11 18:25:36 +0300

    working fixture for filling the database

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,14 +1,33 @@
+import asyncio
 import pytest
-from src.database import Base, async_session_maker, engine
 
-from src.auth.models import Role, User
-from src.goods.models import Goods
-
-@pytest.fixture
-async def prepare_data_base(autouse=True):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+from auth.dao import UserDAO
+from commands.data import goods_data, roles_data, users_data
+from goods.dao import GoodsDAO
+from src.database import async_session_maker, get_async_session
 
 
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_data_base():
+    async for session in get_async_session():
+        await GoodsDAO.clear_goods_table(session)
+        await session.commit()
+        await UserDAO.clear_role_table(session)
+        await session.commit()
+        await UserDAO.clear_user_table(session)
+        await session.commit()
 
+    async with async_session_maker() as session:
+        for role, user in zip(roles_data, users_data):
+            await UserDAO.add_all_roles(session, role)
+            await UserDAO.add_all_users(session, user)
+        for goods in goods_data:
+            await GoodsDAO.add_all_goods(session, goods)
+        await session.commit()
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
