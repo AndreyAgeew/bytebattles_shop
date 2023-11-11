b801a699b03fa396dc6cc519193622a45f19d23f
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-11 21:50:33 +0300

    comments added

--- src/tests/test_users/test_login.py
+++ src/tests/test_users/test_login.py
@@ -4,9 +4,9 @@ import pytest
 
 @pytest.mark.parametrize("email_or_phone,password,status_code",
                          [
-                             ("user@example.com", "Userpassword1!", 200),
-                             ("moderator@example.com", "Moderatorpassword1", 401),
-                             ("+7333333333", "Moderatorpassword1", 404),
+                             ("user@example.com", "Userpassword1!", 200),  # Корректные данные
+                             ("moderator@example.com", "Moderatorpassword1", 401),  # Неверный пароль
+                             ("+7333333333", "Moderatorpassword1", 404),  # Неверный логин
                          ])
 async def test_login_user(email_or_phone, password, status_code, ac: AsyncClient):
     responese = await ac.post("/auth/login", json={
