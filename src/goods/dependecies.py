ebed1d7f888bf5da5a7fb6007ad8dd87c09b5497
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-14 20:21:35 +0300

    added receiving a basket of goods from an authorized user

--- src/goods/dependecies.py
+++ src/goods/dependecies.py
@@ -1,6 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from auth.dependecies import get_current_user
+from auth.models import User
 from database import get_async_session
 from goods.dao import GoodsDAO
 from goods.shopping_cart import ShoppingCart
@@ -11,6 +13,11 @@ async def get_active_goods(session: AsyncSession = Depends(get_async_session)):
     return goods
 
 
-async def get_current_cart():
-    cart = ShoppingCart()
-    return cart
+# Глобальный словарь для хранения связи между пользователем и его корзиной
+user_carts = {}
+
+
+async def get_current_cart(user: User = Depends(get_current_user)):
+    if user.id not in user_carts:
+        user_carts[user.id] = ShoppingCart()
+    return user_carts[user.id]
