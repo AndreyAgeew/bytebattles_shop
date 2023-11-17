8315602c082a23e3d15d859cbd535b445eaa36c3
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-18 01:59:38 +0300

    moved validators to a separate file

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,12 +1,9 @@
-import re
 from typing import Optional
-
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
-
-from auth.errors import PasswordValidationError, PhoneValidationError
 from auth.models import User
 from auth.utils import get_user_db
+from auth.validaters import validate_password, validate_phone
 
 SECRET = "SECRET"
 
@@ -14,23 +11,6 @@ SECRET = "SECRET"
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = SECRET
     verification_token_secret = SECRET
-
-    def __validate_password(self, password: str, confirm_password: str):
-        if password != confirm_password:
-            raise PasswordValidationError("Password and confirm_password do not match")
-        if len(password) < 8:
-            raise PasswordValidationError("Password is too short")
-        if not any(c.isalpha() and c.isascii() for c in password):
-            raise PasswordValidationError("Password must contain Latin characters")
-        if not any(c.isupper() for c in password):
-            raise PasswordValidationError("Password must contain at least one uppercase letter")
-        if not any(c in '$%&!:.' for c in password):
-            raise PasswordValidationError("Password must contain at least one of $%&!:.")
-
-    def __validate_phone(self, phone: str):
-        phone_pattern = re.compile(r'^\+7\d{10}$')
-        if not phone_pattern.match(phone):
-            raise PhoneValidationError("Invalid phone number")
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
@@ -54,8 +34,8 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         )
         password = user_dict.pop("password")
         confirm_password = user_dict.pop("confirm_password")
-        self.__validate_password(password, confirm_password)
-        self.__validate_phone(user_dict['phone_number'])
+        validate_password(password, confirm_password)
+        validate_phone(user_dict['phone_number'])
         user_dict["hashed_password"] = self.password_helper.hash(password)
         user_dict["role_id"] = 1
         created_user = await self.user_db.create(user_dict)
---
+++ src/auth/validaters.py
@@ -0,0 +1,22 @@
+import re
+
+from auth.errors import PasswordValidationError, PhoneValidationError
+
+
+def validate_password(password: str, confirm_password: str):
+    if password != confirm_password:
+        raise PasswordValidationError("Password and confirm_password do not match")
+    if len(password) < 8:
+        raise PasswordValidationError("Password is too short")
+    if not any(c.isalpha() and c.isascii() for c in password):
+        raise PasswordValidationError("Password must contain Latin characters")
+    if not any(c.isupper() for c in password):
+        raise PasswordValidationError("Password must contain at least one uppercase letter")
+    if not any(c in '$%&!:.' for c in password):
+        raise PasswordValidationError("Password must contain at least one of $%&!:.")
+
+
+def validate_phone(phone: str):
+    phone_pattern = re.compile(r'^\+7\d{10}$')
+    if not phone_pattern.match(phone):
+        raise PhoneValidationError("Invalid phone number")
