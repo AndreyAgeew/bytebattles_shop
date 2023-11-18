3c05380a58bd96c2ecef73d7f8af1f9ac62c2f93
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-19 01:17:17 +0300

    The endpoint for creating an order has been changed; an unnecessary scheme has been removed

--- src/order/router.py
+++ src/order/router.py
@@ -7,7 +7,6 @@ from database import get_async_session
 from auth.models import User
 from goods.dao import GoodsDAO
 from .payment import get_stripe_session
-from .schemas import OrderCreate
 from .dao import OrderDAO
 
 router = APIRouter(
@@ -17,27 +16,32 @@ router = APIRouter(
 
 
 @router.post("/create_order")
-async def create_order(order_data: OrderCreate,
-                       user: User = Depends(get_current_user),
-                       session: AsyncSession = Depends(get_async_session)):
-    # Получаем текущую корзину пользователя
-    current_cart = await get_current_cart(user)
+async def create_order(
+        user: User = Depends(get_current_user),
+        session: AsyncSession = Depends(get_async_session)):
+    try:
+        # ваш код для обработки данных и создания заказа
 
-    # Преобразуем basket_history в JSON-совместимый формат
-    basket_history_data = jsonable_encoder(current_cart.items)
+        # Получаем текущую корзину пользователя
+        current_cart = await get_current_cart(user)
 
-    goods = [await GoodsDAO.find_by_id(session=session, goods_id=game['id']) for game in basket_history_data]
-    # Добавляем заказ с использованием OrderDAO
-    await OrderDAO.add_order(session, {
-        'initiator_id': user.id,
-        'basket_history': basket_history_data,
-    })
-    await current_cart.clear_cart()
-    await session.commit()
+        # Преобразуем basket_history в JSON-совместимый формат
+        basket_history_data = jsonable_encoder(current_cart.items)
 
-    # Получаем ссылку на платежную сессию
-    payment_url = get_stripe_session(goods=goods,
-                                     user=user)
+        goods = [await GoodsDAO.find_by_id(session=session, goods_id=game['id']) for game in basket_history_data]
+        # Добавляем заказ с использованием OrderDAO
+        await OrderDAO.add_order(session, {
+            'initiator_id': user.id,
+            'basket_history': basket_history_data,
+        })
+        await current_cart.clear_cart()
+        await session.commit()
+
+        # Получаем ссылку на платежную сессию
+        payment_url = get_stripe_session(goods=goods,
+                                         user=user)
+    except Exception as e:
+        print(f"Error during order creation: {e}")
 
     # Возвращаем JSON-ответ с URL-адресом платежной сессии
     return {"message": "Order created successfully", "payment_url": payment_url}
--- src/order/schemas.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-
-class OrderCreate(BaseModel):
-    initiator_id: int
-    basket_history: dict
