8dea5a9d7282cef54a23689019e64cd3a0b7cf72
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-20 00:40:22 +0300

    add login and registration buttons

---
+++ src/static/js/auth/login_button.js
@@ -0,0 +1,5 @@
+// Обработчик для кнопки логина
+document.getElementById("loginButton").addEventListener("click", function () {
+    // Перенаправление на страницу регистрации
+    window.location.href = "/pages/login";
+});
---
+++ src/static/js/auth/register_button.js
@@ -0,0 +1,5 @@
+// Обработчик для кнопки регистрации
+document.getElementById("registerButton").addEventListener("click", function () {
+    // Перенаправление на страницу регистрации
+    window.location.href = "/pages/register";
+});
