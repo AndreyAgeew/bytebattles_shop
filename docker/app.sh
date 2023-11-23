74017aab3d1c3606173ad0aa2956aaf994118a6a
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-23 15:39:31 +0300

    Add first startup script for initializing database, filling goods table, and running application with Gunicorn.

---
+++ docker/app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 5
+
+alembic upgrade head
+
+python src/commands/fill_goods_table.py
+
+stripe listen --forward-to localhost:7777/webhook
+
+gunicorn src.main:app --workers 4 --worker-clas uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
