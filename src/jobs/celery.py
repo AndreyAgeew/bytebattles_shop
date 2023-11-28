980c34ee272a8f830913f1e727255b644b9898d2
Andrey Ageew <faceofdeviler@gmail.com>
2023-11-28 17:41:40 +0300

    fix app - src for tasks

--- src/jobs/celery.py
+++ src/jobs/celery.py
@@ -5,5 +5,5 @@ from src.config import REDIS_HOST, REDIS_PORT
 celery = Celery(
     "tasks",
     broker=f"redis://{REDIS_HOST}:{REDIS_PORT}",
-    include=["app.tasks.tasks"]
+    include=["src.tasks.tasks"]
 )
